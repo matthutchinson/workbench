936c9de1adabd3fdb31a11c8c24183d429763bcc
Matthew Hutchinson <matt@hiddenloop.com>
2016-02-07 17:24:00 +0000

    minor changes to lifx dash button

--- bin/lifx_dash_button.rb
+++ bin/lifx_dash_button.rb
@@ -1,80 +1,84 @@
 #!/usr/bin/env ruby
 
-# install dependencies with:
-# gem install packetfu lifx-gem fallen --no-ri --no-rdoc
+# install gem dependencies with:
+# gem install packetfu fallen --no-ri --no-rdoc
+# get/set ENV var LIFX_DASH_API_TOKEN at https://cloud.lifx.com/settings
 
 require "packetfu"
-require "lifx"
 require "fallen"
+require "net/http"
+require "net/https"
 
 class LifxDashButton
 
   extend Fallen
 
   def self.run
-    self.new(iface: ARGV[0]).monitor
+    debug = ARGV.any? { |arg| arg.include?('debug') }
+    self.new(iface: ARGV[0], debug: debug).monitor
   end
 
-  MIN_LIFX_LIGHT_COUNT = 4
-  GUARD_TIME_SECONDS   = 5
+  BROADCAST_ARP_DST_IP = "10.0.1.18"
   LIFX_DASH_BUTTON_MAC = "90:72:40:0b:f2:f4"
 
-  def initialize(iface: "en1")
-    @client = LIFX::Client.lan
-    @iface  = iface
-  end
-
-  def find_lights
-    puts "looking for lights ..."
-    @client.discover! { |c| c.lights.count >= MIN_LIFX_LIGHT_COUNT }
-    puts "found lights"
-  rescue LIFX::Client::DiscoveryTimeout
-    puts "sorry, I couldn't find any lights"
+  def initialize(iface: "en1", debug: false)
+    @iface       = iface
+    @debug       = debug
+    @arp_packets = []
   end
 
   def toggle_lights
-    find_lights
+    debug 'toggling lights'
 
-    # always send OFF packets if any are ON
-    if @client.lights.any? { |light| light.on? }
-      puts "#{Time.now} - turning all lights OFF"
-      @client.lights.turn_off
-    else
-      puts "#{Time.now} - turning all lights ON"
-      @client.lights.turn_on
-    end
+    uri = URI("https://api.lifx.com/v1/lights/all/toggle")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    req = Net::HTTP::Post.new(uri)
+    req.add_field "Authorization", "Bearer #{ENV['LIFX_DASH_API_TOKEN']}"
+    res = http.request(req)
 
-    @client.flush
+    debug "response HTTP Status Code: #{res.code}"
+    debug "response HTTP Response Body: #{res.body}"
+  end
+
+  def enough_packets?
+    @arp_packets.select { |p| p == BROADCAST_ARP_DST_IP }.length > 1
   end
 
   def monitor
     # sniff and filter for ARP packets on the interface
     cap = PacketFu::Capture.new(:iface => @iface, :start => true, :filter => "arp")
-    first_arp_at = 0
 
     # parse packet stream and examime src mac address
     cap.stream.each do |packet|
-      break unless self.class.running?
       pkt     = PacketFu::ARPPacket.parse(packet)
       src_mac = PacketFu::EthHeader.str2mac(pkt.eth_src)
 
-      # since we usually get 2 ARP packets at the same time, compare time
+      # since we usually get 2 ARP packets at the same time
       if src_mac == LIFX_DASH_BUTTON_MAC &&
-        Time.now.to_i > (first_arp_at + GUARD_TIME_SECONDS)
         if pkt.arp_opcode == 1
-
-          puts "#{Time.now.to_i} -- #{pkt.peek}"
-          toggle_lights
-          first_arp_at = Time.now.to_i
+          @arp_packets << pkt.arp_dst_ip_readable
+          debug pkt.peek
+          if enough_packets?
+            toggle_lights
+            @arp_packets = []
+          end
         end
       end
     end
   end
+
+  private
+
+  def debug(message)
+    puts "#{Time.now} #{message}" if @debug
+  end
 end
 
-# LifxDashButton.new(iface: ARGV[0]).monitor
+LifxDashButton.run
 
-LifxDashButton.pid_file "/usr/local/var/run/lifx_dash_button.pid"
-LifxDashButton.stdout "/usr/local/var/log/lifx_dash_button.log"
-LifxDashButton.daemonize!
-LifxDashButton.start!
+# LifxDashButton.pid_file "/usr/local/var/run/lifx_dash_button.pid"
+# LifxDashButton.stdout "/usr/local/var/log/lifx_dash_button.log"
+# LifxDashButton.daemonize!
+# LifxDashButton.start!
