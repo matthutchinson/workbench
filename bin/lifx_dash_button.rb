ee7786047cd3ff8ba15a0ef12880be00f3d0eb7d
Matthew Hutchinson <matt@hiddenloop.com>
2016-03-24 22:46:59 +0000

    lifx dash button refactor

--- bin/lifx_dash_button.rb
+++ bin/lifx_dash_button.rb
@@ -2,65 +2,94 @@
 
 # install gem dependencies with:
 # gem install packetfu --no-ri --no-rdoc
-# get/set ENV var LIFX_DASH_API_TOKEN at https://cloud.lifx.com/settings
 
 require "packetfu"
-require "net/http"
 require "net/https"
 
-class LifxDashButton
+module LifxDash
+  class Toggle
 
-  def self.run(iface = "en1", lifx_token = '')
-    self.new(iface, lifx_token).monitor
-  end
+    def initialize(token, selector)
+      @token    = token
+      @selector = selector
+    end
 
-  LIFX_DASH_BUTTON_MAC = "f0:4f:7c:99:8a:73"
+    def toggle
+      log "[POST] #{uri}"
 
-  def initialize(iface, lifx_token)
-    @iface = iface
-    @lifx_token = lifx_token
-  end
+      Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+        req = Net::HTTP::Post.new(uri)
+        req.add_field "Authorization", "Bearer #{@token}"
+        res = http.request(req)
 
-  def toggle_lights
-    log 'toggling lights'
-
-    uri = URI("https://api.lifx.com/v1/lights/all/toggle")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    req = Net::HTTP::Post.new(uri)
-    req.add_field "Authorization", "Bearer #{@lifx_token}"
-    res = http.request(req)
-
-    log "response HTTP Status Code: #{res.code}"
-    log "response HTTP Response Body: #{res.body}"
-  end
-
-  def monitor
-    # sniff and filter for ARP packets on the interface
-    cap = PacketFu::Capture.new(:iface => @iface, :start => true, :filter => "arp")
-
-    # parse packet stream and examime src mac address
-    cap.stream.each do |packet|
-      pkt     = PacketFu::ARPPacket.parse(packet)
-      src_mac = PacketFu::EthHeader.str2mac(pkt.eth_src)
-
-      # pressing the button, we usually get 2 good ARP packets within 5-10
-      # seconds of each other, so we flush out others and look for these
-      if src_mac == LIFX_DASH_BUTTON_MAC
-        if pkt.arp_opcode == 1
-          log pkt.peek
-          toggle_lights
-        end
+        log "API reply (#{res.code}): #{res.body}"
       end
+    end
+
+    private
+
+    def log(message)
+      puts "#{Time.now} - #{self.class} - #{message}"
+    end
+
+    def uri
+      @uri ||= URI("https://api.lifx.com/v1/lights/#{@selector}/toggle")
     end
   end
 
-  private
 
-  def log(message)
-    puts "#{Time.now} #{message}"
+  class Monitor
+
+    def initialize(mac_address, iface)
+      @mac_address = mac_address
+      @iface = iface
+    end
+
+    def listen(&block)
+      # listen to (and parse) packets
+      capture.stream.each do |packet|
+        pkt = PacketFu::ARPPacket.parse(packet)
+        mac = PacketFu::EthHeader.str2mac(pkt.eth_src)
+
+        # check mac address matches and arp opcode is 1
+        if mac == @mac_address
+          if pkt.arp_opcode == 1
+            log pkt.peek
+            block.call(pkt)
+          end
+        end
+      end
+    end
+
+    private
+
+    def log(message)
+      puts "#{Time.now} - #{self.class} - #{message}"
+    end
+
+    def capture
+      # sniff and filter for ARP packets on the interface
+      @capture ||= PacketFu::Capture.new(
+        :iface => @iface,
+        :start => true,
+        :filter => "arp"
+      )
+    end
+  end
+
+  # set up monitor and toggle, then listen for packets
+  def self.run(options = {})
+    monitor = LifxDash::Monitor.new(options[:mac_address], options.fetch(:iface, "en0"))
+    lifx = LifxDash::Toggle.new(options[:lifx_token], options.fetch(:lifx_selector, "all"))
+
+    monitor.listen { |pkt| lifx.toggle }
   end
 end
 
-LifxDashButton.run(ARGV[0] || 'en1', ARGV[1])
+
+LifxDash.run(
+  iface: "en0",
+  mac_address: "f0:4f:7c:99:8a:73",
+  lifx_token: "c2533f027a1d073ff5cb34cb77ea1e0ae6256c5a290ae7fe2d9bba3698af769d",
+  lifx_selector: "d073d501ffd4"
+)
