6b40a0b153a0498f7969952b6b8a340b534c575f
Matthew Hutchinson <matt@hiddenloop.com>
2026-07-09 17:59:25 +0100

    lsp nvim stuff

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,19 @@ return {
   -- do we need to nest the table {}?
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+	"folke/lazydev.nvim",
+	ft = "lua", -- only load on lua files
+	opts = {
+	  library = {
+	    -- See the configuration section for more details
+	    -- Load luvit types when the `vim.uv` word is found
+	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+	  },
+	},
+      },
+    },
     config = function()
       vim.lsp.enable('lua_ls')
       vim.lsp.enable('ruby_lsp')
