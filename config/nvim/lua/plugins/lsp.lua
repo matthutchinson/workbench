cd585b24ee2aebb5b4ccb2791192e9d4d5380072
Matthew Hutchinson <matt@hiddenloop.com>
2026-07-13 00:00:34 +0100

    set up editor config, lsp code formatting on file saves, and ff

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -2,22 +2,42 @@ return {
   -- do we need to nest the table {}?
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-	"folke/lazydev.nvim",
-	ft = "lua", -- only load on lua files
-	opts = {
-	  library = {
-	    -- See the configuration section for more details
-	    -- Load luvit types when the `vim.uv` word is found
-	    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-	  },
-	},
+    dependencies = { {
+      "folke/lazydev.nvim",
+      ft = "lua", -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
       },
     },
+    },
     config = function()
-      vim.lsp.enable('lua_ls')
-      vim.lsp.enable('ruby_lsp')
+      vim.lsp.enable('lua_ls')   -- brew install lua-language-server
+      vim.lsp.enable('ruby_lsp') -- gem install ruby-lsp
+
+      -- e.g  ff to format the current buffer (in normal mode) would be
+      vim.keymap.set("n", "ff", function() vim.lsp.buf.format() end)
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+          if not client then return end
+
+          if not client:supports_method('textDocument/willSaveWaitUntil')
+              and client:supports_method('textDocument/formatting') then
+            -- when supported, always format the current buffer on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+              end,
+            })
+          end
+        end,
+      })
     end,
   }
 }
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,12 @@
 return {
   {
+    -- also npm install -g tree-sitter-cli
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter').install { 
-	"lua", "ruby", "javascript", "html", "css", "vim", "vimdoc", "markdown", "c", "json",
+      require('nvim-treesitter').install {
+        "lua", "ruby", "javascript", "html", "css", "vim", "vimdoc", "markdown", "c", "json",
       }
     end
   }
