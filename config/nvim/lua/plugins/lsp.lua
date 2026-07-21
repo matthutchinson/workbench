cfa9212a58bb2154ea2c3c1ac681334bf0351d31
Matthew Hutchinson <matt@hiddenloop.com>
2026-07-20 22:02:44 -0400

    nvim updates

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -18,10 +18,11 @@ return {
       vim.lsp.enable('lua_ls')   -- brew install lua-language-server
       vim.lsp.enable('ruby_lsp') -- gem install ruby-lsp
 
-      -- e.g  ff to format the current buffer (in normal mode) would be
+      -- e.g ff to format the current buffer (in normal mode) would be
       vim.keymap.set("n", "ff", function() vim.lsp.buf.format() end)
 
       vim.api.nvim_create_autocmd('LspAttach', {
+        -- note no buffer arg, happing on all attaches in nv
         callback = function(args)
           local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
           if not client then return end
@@ -29,10 +30,12 @@ return {
           if not client:supports_method('textDocument/willSaveWaitUntil')
               and client:supports_method('textDocument/formatting') then
             -- when supported, always format the current buffer on save
+            -- or also, you can check ft in here with
+            -- if vim.bo.filtetype == "lua" == "lua"
             vim.api.nvim_create_autocmd('BufWritePre', {
               buffer = args.buf,
               callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1500 })
               end,
             })
           end
